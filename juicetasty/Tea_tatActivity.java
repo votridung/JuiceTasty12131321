e2ddf3f29a164e5f93f0a333a42857aafd267147
votridung <31879537+votridung@users.noreply.github.com>
2017-10-21 18:55:55 +0700

    Add files via upload

---
+++ juicetasty/Tea_tatActivity.java
@@ -0,0 +1,17 @@
+package gamemobile.com.juicetasty;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Asus on 19/10/2017.
+ */
+
+public class Tea_tatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tea_tat);
+    }
+}
